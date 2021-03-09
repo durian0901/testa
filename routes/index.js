cff79adccc0583d8fade9b47f6867859fb8049aa
testb <01002096@gmail.com>
2021-03-09 20:26:59 +0800

    0309a

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+// home page
 router.get('/', function(req, res, next) {
 
   var db = req.con;
   var data = "";
 
-  db.query('SELECT * FROM account',function(err,rows){
-    if(err){
-      console.log(err);
-    }
-    var data=rows;
+  var user = "";
+  var user = req.query.user;
 
-    res.render('index', { title: 'Account Information',data: data });
+  var filter = "";
+  if (user) {
+      filter = 'WHERE userid = ?';
+  }
+
+  db.query('SELECT * FROM account ' + filter, user, function(err, rows) {
+      if (err) {
+          console.log(err);
+      }
+      var data = rows;
+
+      // use index.ejs
+      res.render('index', { title: 'Account Information', data: data, user: user });
   });
-  
+
 });
 
   //use userAdd.ejs
@@ -43,42 +52,55 @@ router.post('/userAdd',function(req, res, next){
     });
 });
 
-function Edit(id){
-  window.location.href="/userEdit?id="+id;
-}
+// edit page
+router.get('/userEdit', function(req, res, next) {
 
-router.get('/userEdit',function(req, res, next){
+  var id = req.query.id;
+  var db = req.con;
+  var data = "";
 
-  var id=req.query.id;
-  var db=req.con;
-  var data="";
+  db.query('SELECT * FROM account WHERE id = ?', id, function(err, rows) {
+      if (err) {
+          console.log(err);
+      }
 
-  db.query('SELECT * FROM account WHERE id= ?',id, function(err, rows){
-    if(err){
-      console.log(err);
-    }
-
-    var data =rows;
-    res.render('userEdit',{title:'Edit Account',data: data });
+      var data = rows;
+      res.render('userEdit', { title: 'Edit Account', data: data });
   });
 
 });
 
-router.post('/userEdit',function(req, res, next){
-  var db=req.con;
-  var id=req.body.id;
-  var sql={
-    userid: req.body.password,
-    password: req.body.password,
-    email:req.body.email
+router.post('/userEdit', function(req, res, next) {
+
+  var db = req.con;
+  var id = req.body.id;
+
+  var sql = {
+      userid: req.body.userid,
+      password: req.body.password,
+      email: req.body.email
   };
 
-  var qur= db.query('UPDATE account SET ? WHERE id =?',[sql,id], function(err, rows){
-      if(err){
-        console.log(err);
+  var qur = db.query('UPDATE account SET ? WHERE id = ?', [sql, id], function(err, rows) {
+      if (err) {
+          console.log(err);
       }
 
-      res.setHeader('Content-Type','application/json');
+      res.setHeader('Content-Type', 'application/json');
+      res.redirect('/');
+  });
+
+});
+
+router.get('/userDelete', function(req, res, next) {
+
+  var id = req.query.id;
+  var db = req.con;
+
+  var qur = db.query('DELETE FROM account WHERE id = ?', id, function(err, rows) {
+      if (err) {
+          console.log(err);
+      }
       res.redirect('/');
   });
 });
